1063b730ff2f2499a3438e1b4721f9a4d2b7b853
bcs44 <bruna_silva_05@hotmail.com>
2019-05-18 10:19:15 +0100

    GetService

---
+++ app/src/main/java/mestrado/ipg/mcmstore/service/BackgroundPostService.java
@@ -0,0 +1,16 @@
+package mestrado.ipg.mcmstore.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BackgroundPostService extends Service {
+    public BackgroundPostService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
